8c37eeccfcdb769700fd752ee51f5a6283a63cda
Matthew Dillon <matthewrdillon@gmail.com>
2020-05-15 11:10:34 -0700

    SQUASH

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-# ----------------------------------------------------------------------------
-# Copyright (c) 2020, QIIME 2 development team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file LICENSE, distributed with this software.
-# ----------------------------------------------------------------------------
-
 from setuptools import find_packages, setup
 
 import versioneer
