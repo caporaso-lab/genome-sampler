5f44405269eb02e4c56a6c1378e4ad489ab01e8c
Evan Bolyen <ebolyen@gmail.com>
2020-05-15 10:39:48 -0700

    INIT: QIIME 2 plugin structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+setup(
+    name='q2-covid-19',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='BSD-3-Clause',
+    packages=find_packages(),
+    author="Evan Bolyen",
+    author_email="ebolyen@gmail.com",
+    description="TODO",
+    url="https://github.com/caporaso-lab/q2-covid-19",
+    entry_points={
+        'qiime2.plugins':
+        ['q2-covid-19=q2_covid_19.plugin_setup:plugin']
+    },
+    package_data={
+        'q2_covid_19': ['citations.bib']
+    },
+    zip_safe=False,
+)
