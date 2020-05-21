7ba0060811a2261c68233259beedfd4f8bbc4d5b
Greg Caporaso <gregcaporaso@gmail.com>
2020-05-21 12:57:52 -0700

    ENH: adds GISAID format and transformer

--- q2_covid/common.py
+++ q2_covid/common.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+import re
+
 import qiime2
 import qiime2.plugin.model as model
 from qiime2.plugin import SemanticType
 
 from q2_types.feature_data import FeatureData
+from qiime2.plugin import ValidationError
 
 
 class UNIXListFormat(model.TextFileFormat):
@@ -45,3 +48,73 @@ class IDSelection:
 
 
 Selection = SemanticType('Selection', variant_of=FeatureData.field['type'])
+
+# modified from DNAFastaFormat in q2_types to allow lowercase characters
+# https://github.com/qiime2/q2-types/blob/058ee0e40e38edaa02b1aad034df37456aeb4ddf/q2_types/feature_data/_format.py#L146
+class GISAIDDNAFASTAFormat(model.TextFileFormat):
+    def _validate_lines(self, max_lines):
+        FASTADNAValidator = re.compile(r'[ACGTURYKMSWBDHVNacgturykmswbdhvn\- ]+\r?\n?')
+        ValidationSet = frozenset(('A', 'C', 'G', 'T', 'U', 'R', 'Y', 'K', 'M',
+                                   'S', 'W', 'B', 'D', 'H', 'V', 'N', 'a', 'c',
+                                   'g', 't', 'u', 'r', 'y', 'k', 'm', 's', 'w',
+                                   'b', 'd', 'h', 'v', 'n', '-', ' '))
+
+        last_line_was_ID = False
+        ids = {}
+
+        with open(str(self), 'rb') as fh:
+            try:
+                first = fh.read(6)
+                if first[:3] == b'\xEF\xBB\xBF':
+                    first = first[3:]
+                # Empty files should validate
+                if first.strip() == b'':
+                    return
+                if first[0] != ord(b'>'):
+                    raise ValidationError("First line of file is not a valid "
+                                          "description. Descriptions must "
+                                          "start with '>'")
+                fh.seek(0)
+                for line_number, line in enumerate(fh, 1):
+                    line = line.strip()
+                    if line_number >= max_lines:
+                        return
+                    line = line.decode('utf-8-sig')
+                    if line.startswith('>'):
+                        if last_line_was_ID:
+                            raise ValidationError('Multiple consecutive '
+                                                  'descriptions starting on '
+                                                  f'line {line_number-1!r}')
+                        line = line.split()
+                        if line[0] == '>':
+                            if len(line) == 1:
+                                raise ValidationError(
+                                    f'Description on line {line_number} is '
+                                    'missing an ID.')
+                            else:
+                                raise ValidationError(
+                                    f'ID on line {line_number} starts with a '
+                                    'space. IDs may not start with spaces')
+                        if line[0] in ids:
+                            raise ValidationError(
+                                f'ID on line {line_number} is a duplicate of '
+                                f'another ID on line {ids[line[0]]}.')
+                        ids[line[0]] = line_number
+                        last_line_was_ID = True
+                    elif re.fullmatch(FASTADNAValidator, line):
+                        last_line_was_ID = False
+                    else:
+                        for position, character in enumerate(line):
+                            if character not in ValidationSet:
+                                raise ValidationError(
+                                    f"Invalid character '{character}' at "
+                                    f"position {position} on line "
+                                    f"{line_number} (does not match IUPAC "
+                                    "characters for a DNA sequence).")
+            except UnicodeDecodeError as e:
+                raise ValidationError(f'utf-8 cannot decode byte on line '
+                                      f'{line_number}') from e
+
+    def _validate_(self, max_lines):
+        level_map = {'min': 100, 'max': float('inf')}
+        self._validate_lines(level_map[max_lines])
--- q2_covid/plugin_setup.py
+++ q2_covid/plugin_setup.py
@@ -1,11 +1,17 @@
 import qiime2
 from qiime2.plugin import Plugin, Metadata, Int, Range
 
-from q2_types.feature_data import FeatureData
+import skbio
+import tempfile
+
+from q2_types.feature_data import (
+        FeatureData, DNAIterator, DNAFASTAFormat, 
+        DNASequencesDirectoryFormat)
 
 import q2_covid
 from q2_covid.common import (IDSelectionDirFmt, IDSelection, Selection,
-                             IDMetadataFormat, UNIXListFormat)
+                             IDMetadataFormat, UNIXListFormat, 
+                             GISAIDDNAFASTAFormat)
 from q2_covid.subsample_random import subsample_random
 
 plugin = Plugin(
@@ -13,12 +19,12 @@ plugin = Plugin(
     website='https://github.com/caporaso-lab/q2-covid',
     package='q2_covid',
     version=q2_covid.__version__,
-    description='Tools for genomic epidemiology focused on the SARS-CoV-2'
-                ' virus.',
-    short_description='Tools for genomic epidemiology.'
+    description='Tools for sampling collections of genomes.',
+    short_description='Genome sampler.'
 )
 
 plugin.register_formats(IDSelectionDirFmt)
+plugin.register_formats(GISAIDDNAFASTAFormat)
 plugin.register_semantic_types(Selection)
 plugin.register_semantic_type_to_format(FeatureData[Selection],
                                         artifact_format=IDSelectionDirFmt)
@@ -49,6 +55,42 @@ def _1(obj: IDSelection) -> IDSelectionDirFmt:
 def _2(fmt: IDSelectionDirFmt) -> qiime2.Metadata:
     md = fmt.metadata.view(IDMetadataFormat).to_metadata()
     return md.filter_ids(fmt.included.view(UNIXListFormat).to_list())
+
+
+def _read_gisaid_dna_fasta(path):
+    def _cleanup_gen():
+        with open(path) as input_f:
+            for line in input_f:
+                if line.startswith('>'):
+                    yield line
+                else:
+                    # Due to a bug in skbio 0.5.5, the lowercase option can't
+                    # be used with skbio.io.read for reading DNA sequences. 
+                    # Convert sequences to uppercase here.
+                    line = line.upper()
+                    # Spaces and gap characters can appear in unaligned GISAID
+                    # sequence records, so we strip those.
+                    line = line.replace('-', '')
+                    line = line.replace('.', '')
+                    line = line.replace(' ', '')
+                    yield line 
+
+    result = skbio.io.read(_cleanup_gen(), verify=False,
+                           format='fasta', constructor=skbio.DNA)
+    return result
+
+
+@plugin.register_transformer
+def _3(fmt: GISAIDDNAFASTAFormat) -> DNASequencesDirectoryFormat:
+    data = _read_gisaid_dna_fasta(str(fmt))
+    df = DNASequencesDirectoryFormat()
+    ff = DNAFASTAFormat()
+
+    with ff.open() as file:
+        skbio.io.write(data, format='fasta', into=file)
+
+    df.file.write_data(ff, DNAFASTAFormat)
+    return df
 
 
 plugin.methods.register_function(
