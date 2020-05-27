93236ec09a7c65ea84fd00f4dcaad1f2a4105148
Greg Caporaso <gregcaporaso@gmail.com>
2020-05-27 16:18:28 -0700

    squash
    pair-programming with @ebolyen

--- q2_covid/subsample_longitudinal.py
+++ q2_covid/subsample_longitudinal.py
@@ -6,24 +6,45 @@ import qiime2
 from q2_covid.common import IDSelection
 
 
-def subsample_longitudinal(time: qiime2.CategoricalMetadataColumn,
-                           start: str,
-                           rate_n: int = 7,
-                           rate_duration: int = 7) -> IDSelection:
+def _sample_group(samples_per_interval):
+    def _sampler(df):
+        df = df.dropna(axis=0)
+        if len(df) > samples_per_interval:
+            return df.sample(samples_per_interval)
+        else:
+            return df
+    
+    return _sampler
 
-    window_size = '%dD' % rate_duration
-    filter_before = pd.Timestamp(start)
 
-    dt_series = pd.to_datetime(time.to_series(), errors='coerce')
-    df = pd.DataFrame({'ids': time.to_series().index}, index=dt_series)
+def subsample_longitudinal(dates: qiime2.CategoricalMetadataColumn,
+                           start_date: str = None,
+                           samples_per_interval: int = 7,
+                           days_per_interval: int = 7) -> IDSelection:
 
-    df = df.iloc[np.where(dt_series > filter_before)]
-    grouped = df.groupby(pd.Grouper(freq=window_size), group_keys=False)
-    filtered_df = grouped.apply(
-        lambda g: g.sample(rate_n) if len(g) > rate_n else g)
+    window_size = '%dD' % days_per_interval
 
-    selection = pd.Series(False, index=df['ids'])
+    dt_series = pd.to_datetime(dates.to_series(), errors='coerce')
+    df = pd.DataFrame({'ids': dates.to_series().index}, index=dt_series)
+
+    if start_date is not None:
+        filter_before = pd.Timestamp(start_date)
+        df = df.iloc[np.where(dt_series >= filter_before)]
+        if filter_before not in df.index:
+            # this will be stripped in _sample_group::_sampler
+            # the purpose is to force Pandas to begin the window at this
+            # time instead of the first observation (by making NaN the first
+            # observation)
+            df.loc[filter_before] = float('nan')
+
+    grouped = df.groupby(pd.Grouper(freq=window_size, convention='start', 
+                                    closed='left'), 
+                         group_keys=False)
+    filtered_df = grouped.apply(_sample_group(samples_per_interval))
+
+    df = df.dropna(axis=0)
+    selection = pd.Series(False, index=dates.to_series().index)
     selection[filtered_df['ids']] = True
 
-    md = qiime2.Metadata(time.to_dataframe())
+    md = qiime2.Metadata(dates.to_dataframe())
     return IDSelection(selection, md, 'subsample_longitudinal')
