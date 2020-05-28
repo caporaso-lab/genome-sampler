9520dac3bbfe4377b142f721cf96d84c4470df21
Greg Caporaso <gregcaporaso@gmail.com>
2020-05-28 10:18:31 -0700

    API: renamed parameter of filter-seqs

--- q2_covid/filter.py
+++ q2_covid/filter.py
@@ -16,15 +16,15 @@ from q2_types.feature_data import DNAIterator
 def filter_seqs(sequences: DNAIterator,
                 min_length: int=1,
                 max_length: int=None,
-                max_ambiguous_fraction: float=1.0) -> pd.Series:
+                max_proportion_ambiguous: float=1.0) -> pd.Series:
     max_length = max_length or np.inf
     result = {}
     for sequence in sequences:
         sequence_length = len(sequence)
-        ambiguous_fraction = sequence.degenerates().sum() / sequence_length
+        ambiguous_proportion = sequence.degenerates().sum() / sequence_length
         too_long = sequence_length > max_length
         too_short = sequence_length < min_length
-        too_ambiguous = ambiguous_fraction > max_ambiguous_fraction
+        too_ambiguous = ambiguous_proportion > max_proportion_ambiguous
         if not (too_short or too_long or too_ambiguous):
             result[sequence.metadata['id']] = sequence
     return pd.Series(result)
