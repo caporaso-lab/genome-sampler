251261c4f6c1e440f3dfa5b24e85dbbd28e0d429
Greg Caporaso <gregcaporaso@gmail.com>
2020-05-28 10:14:25 -0700

    NEW: adds GISAID format and transformer (#4)

--- q2_covid/plugin_setup.py
+++ q2_covid/plugin_setup.py
@@ -1,24 +1,31 @@
 import qiime2
-from qiime2.plugin import Plugin, Metadata, Int, Range
+from qiime2.plugin import Plugin, Metadata, Int, Range, Float
 
-from q2_types.feature_data import FeatureData
+import skbio
+import tempfile
+
+from q2_types.feature_data import (
+        FeatureData, DNAIterator, DNAFASTAFormat, 
+        DNASequencesDirectoryFormat, Sequence)
 
 import q2_covid
 from q2_covid.common import (IDSelectionDirFmt, IDSelection, Selection,
-                             IDMetadataFormat, UNIXListFormat)
+                             IDMetadataFormat, UNIXListFormat, 
+                             GISAIDDNAFASTAFormat)
 from q2_covid.subsample_random import subsample_random
+from q2_covid.filter import filter_seqs
 
 plugin = Plugin(
     name='covid',
     website='https://github.com/caporaso-lab/q2-covid',
     package='q2_covid',
     version=q2_covid.__version__,
-    description='Tools for genomic epidemiology focused on the SARS-CoV-2'
-                ' virus.',
-    short_description='Tools for genomic epidemiology.'
+    description='Tools for sampling collections of genomes.',
+    short_description='Genome sampler.'
 )
 
 plugin.register_formats(IDSelectionDirFmt)
+plugin.register_formats(GISAIDDNAFASTAFormat)
 plugin.register_semantic_types(Selection)
 plugin.register_semantic_type_to_format(FeatureData[Selection],
                                         artifact_format=IDSelectionDirFmt)
@@ -51,6 +58,42 @@ def _2(fmt: IDSelectionDirFmt) -> qiime2.Metadata:
     return md.filter_ids(fmt.included.view(UNIXListFormat).to_list())
 
 
+def _read_gisaid_dna_fasta(path):
+    def _cleanup_gen():
+        with open(path) as input_f:
+            for line in input_f:
+                if line.startswith('>'):
+                    yield line
+                else:
+                    # Due to a bug in skbio 0.5.5, the lowercase option can't
+                    # be used with skbio.io.read for reading DNA sequences. 
+                    # Convert sequences to uppercase here.
+                    line = line.upper()
+                    # Spaces and gap characters can appear in unaligned GISAID
+                    # sequence records, so we strip those.
+                    line = line.replace('-', '')
+                    line = line.replace('.', '')
+                    line = line.replace(' ', '')
+                    yield line 
+
+    result = skbio.io.read(_cleanup_gen(), verify=False,
+                           format='fasta', constructor=skbio.DNA)
+    return result
+
+
+@plugin.register_transformer
+def _3(fmt: GISAIDDNAFASTAFormat) -> DNASequencesDirectoryFormat:
+    data = _read_gisaid_dna_fasta(str(fmt))
+    df = DNASequencesDirectoryFormat()
+    ff = DNAFASTAFormat()
+
+    with ff.open() as file:
+        skbio.io.write(data, format='fasta', into=file)
+
+    df.file.write_data(ff, DNAFASTAFormat)
+    return df
+
+
 plugin.methods.register_function(
     function=subsample_random,
     inputs={},
@@ -71,4 +114,31 @@ plugin.methods.register_function(
     },
     name='Randomly sample IDs',
     description='Randomly sample IDs without replacement.'
+)
+
+
+plugin.methods.register_function(
+    function=filter_seqs,
+    inputs={'sequences': FeatureData[Sequence]},
+    parameters={
+        'min_length': Int % Range(1, None),
+        'max_length': Int % Range(1, None),
+        'max_ambiguous_fraction': Float % Range(0, 1, inclusive_end=True)
+    },
+    outputs=[('filtered_sequences', FeatureData[Sequence])],
+    parameter_descriptions={
+        'min_length': ('The minimum length of a sequence that will allow' 
+                       ' it be retained.'),
+        'max_length': ('The maximum length of a sequence that will allow'
+                       ' the sequence to be retained.'),
+        'max_ambiguous_fraction': 
+         ('The maximum fraction of sequence characters that can be ambiguous'
+          ' (e.g., N) that will allow it be retained.')
+    },
+    input_descriptions={'sequences': 'The sequences to be filtered.'},
+    output_descriptions={
+        'filtered_sequences': 'The sequences retained after filtering.'
+    },
+    name='Filter sequences.',
+    description='Filter sequences based on their length and ambiguity.'
 )
---
+++ q2_covid/tests/test_transformers.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+import os.path
+
+import skbio
+
+from qiime2.plugin.testing import TestPluginBase
+from q2_types.feature_data import DNASequencesDirectoryFormat
+
+from ..plugin_setup import GISAIDDNAFASTAFormat
+
+
+class TestGISAIDDNAFASTAFormatTransformerTests(TestPluginBase):
+    package = 'q2_covid.tests'
+
+    def test_gisaid_dna_fasta_format_to_dna_iterator(self):
+        input, obs = self.transform_format(GISAIDDNAFASTAFormat, 
+                                           DNASequencesDirectoryFormat,
+                                           filename='gisaid1.fasta')
+        
+        obs_fp = os.path.join(str(obs), 'dna-sequences.fasta')
+        obs = skbio.io.read(obs_fp, format='fasta', constructor=skbio.DNA)
+        obs = list(obs)
+        
+        self.assertEqual(len(obs), 3)
+
+        self.assertEqual(obs[0].metadata['id'], 'USA/AZ-TGXXXX/2020')
+        self.assertEqual(str(obs[0]), 'ACGTNTGCATNACANTGCTANNNNNNNN')
+
+        self.assertEqual(obs[1].metadata['id'], 'pangolin/Asdf/JKL/2017')
+        self.assertEqual(str(obs[1]), 'ACGTGACCANNNNNNNNNACGTCAGTACAGTACCANN')
+
+        self.assertEqual(obs[2].metadata['id'], 'NC_045512.2')
+        self.assertEqual(len(obs[2]), 29903)
