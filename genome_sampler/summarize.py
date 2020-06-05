1f1487d0360e18abb899c6f3e6f50001285157ab
Evan Bolyen <ebolyen@gmail.com>
2020-06-05 11:49:00 -0700

    squash: remove altair

--- genome_sampler/summarize.py
+++ genome_sampler/summarize.py
@@ -1,4 +1,3 @@
-import os
 import pkg_resources
 
 import pandas as pd
@@ -22,32 +21,11 @@ def _build_summary_table(selections):
     return table
 
 
-def _make_longitudinal_plot(dates):
-    # TODO: return an altair plot for this.
-    pass
-
-def _merge_time_to_dates(selections):
-    to_merge = []
-    for selection in selections:
-        if selection.label == 'sample_longitudinal':
-            to_merge.append(selection)
-
-    if not to_merge:
-        return None
-
-
-
 def summarize_selection(output_dir: str, selections: IDSelection):
     context = {}
 
     table = _build_summary_table(selections)
     html_table = q2templates.df_to_html(table, index=False)
     context['table'] = html_table
-
-    dates = _merge_time_to_dates(selections)
-    if dates is not None:
-        longitudinal_plot = _make_longitudinal_plot(dates)
-        longitudinal_plot.save(os.path.join(output_dir, 'longitudinal.html'))
-        context['longitudinal_plot'] = True
 
     q2templates.render(SUMMARY_TEMPLATE, output_dir, context=context)
